4a0a568f675b1be936326accc5d0cc6a79ed82db
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2019-05-21 13:05:01 +0200

    Update boost build script

--- boost.sh
+++ boost.sh
@@ -3,12 +3,24 @@
 # -----------------------------------------------------
 # | INSTALLER SCRIPT: boost
 # | > https://docs.opencv.org/4.1.0/d7/d9f/tutorial_linux_install.html
+# | > https://www.osetc.com/en/how-to-install-boost-on-ubuntu-16-04-18-04-linux.html
 # -----------------------------------------------------
 
+# Install the basics to run boost build
 sudo apt-get update
-sudo apt-get install build-essential git
+sudo apt-get install build-essential
 
-git clone --recursive https://github.com/boostorg/boost.git
-cd boost
+# Download source package of boost
+wget https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz
+# Extract all files from the archive file
+tar -zxvf boost_1_70_0.tar.gz
+rm boost_1_70_0.tar.gz
+
+# Build and install boost
+cd boost_1_70_0
 ./bootstrap.sh
-./b2 headers
+./b2
+
+# Remove extracted directory
+cd ..
+rm -rf boost_1_70_0
